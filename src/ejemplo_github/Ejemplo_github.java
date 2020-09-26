ad58125d7e5f0eef782b1c238ed11ddf5e04b196
diego0023 <diegogalan.0023@gmail.com>
2020-09-25 20:32:40 -0600

    sumar 2 numeros

--- src/ejemplo_github/Ejemplo_github.java
+++ src/ejemplo_github/Ejemplo_github.java
@@ -16,9 +16,9 @@ public class Ejemplo_github {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int a;
-        int b;
-        
+        int a=0;
+        int b=2;
+        System.out.println(a+b);
       
     }
     
